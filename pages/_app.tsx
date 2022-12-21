45472d12014a9a2f73e3b84c0519db821bae84e6
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-12-21 13:31:20 +0545

    build: added react-redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../lib/store/store';
 import './globals.css';
 import { NextPageWithLayout } from './page';
 
@@ -10,7 +12,11 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />)
+    </Provider>
+  );
 }
 
 export default MyApp;
